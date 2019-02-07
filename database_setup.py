131a272fefbd028db9ad2173a36d262435c6a215
Aleksandr Zonis <zonis7@gmail.com>
2019-02-07 11:40:14 -0600

    Make addproducts and database_setup PEP8 compliant

--- database_setup.py
+++ database_setup.py
@@ -6,21 +6,21 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 
 class User(Base):
-  __tablename__= 'user'
+    __tablename__ = 'user'
 
-  id = Column(Integer, primary_key = True)
-  name = Column(String(500), nullable = False)
-  email = Column(String(500), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(500), nullable=False)
+    email = Column(String(500), nullable=False)
 
 
 class Category(Base):
     __tablename__ = 'category'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False, unique=True)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -28,47 +28,43 @@ class Category(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 
 class Product(Base):
     __tablename__ = 'product'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     image_url = Column(String(250))
     product_url = Column(String(250))
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
-    category_id = Column(Integer,ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     # When Category is deleted, all products that it contains
     # will get deleted as well
-    category = relationship(Category, 
+    category = relationship(Category,
                             backref=backref("products", cascade="all,delete"))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
-    
-    
+
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'image_url'         : self.image_url,
-           'product_url'         : self.product_url,
-           'category_id'         : self.category_id
-       }
-
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'description': self.description,
+           'id': self.id,
+           'image_url': self.image_url,
+           'product_url': self.product_url,
+           'category_id': self.category_id
+        }
 
 
 engine = create_engine('sqlite:///furniturecatalog.db')
- 
 
 Base.metadata.create_all(engine)
