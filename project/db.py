26eedd0b867e71aa81d891439b1fa1e8960cc0a0
Aleksandr Zonis <zonis7@gmail.com>
2019-03-14 12:51:42 -0500

    Make db.py PEP8 compliant

--- project/db.py
+++ project/db.py
@@ -6,13 +6,12 @@ from models import Base
 
 class DBConnector():
 
-	engine = create_engine('sqlite:///furniturecatalog.db', 
-		                   connect_args={'check_same_thread': False})
-	Base.metadata.bind = engine
+    engine = create_engine('sqlite:///furniturecatalog.db',
+                           connect_args={'check_same_thread': False})
+    Base.metadata.bind = engine
 
-	def __init__(self):
-		self.DBSession = sessionmaker(bind=self.engine)
-		
+    def __init__(self):
+        self.DBSession = sessionmaker(bind=self.engine)
 
-	def get_session(self):
-		return self.DBSession()
+    def get_session(self):
+        return self.DBSession()
